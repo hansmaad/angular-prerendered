f6ca53cffc779c820af7a0c121dea9ef73536ab7
Hannes Kamecke <hansmaad@users.noreply.github.com>
2018-11-02 15:57:05 +0100

    Avoid flicker on prerendered lazy loaded modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
     HomeComponent
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
     BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
   providers: [],
